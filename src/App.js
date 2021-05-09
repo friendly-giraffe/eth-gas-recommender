a274eb2a4e89b383423e229187a867354509673c
Christian Wilkie <christian.wilkie@gmail.com>
2021-05-09 00:00:37 -0400

    vanilla bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Col, Row, Container} from "react-bootstrap";
+import {Alert} from "bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+              <h1 className="display-4">Fluid jumbotron</h1>
+              <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its
+                  parent.</p>
+          </div>
+      </div>
   );
 }
 
